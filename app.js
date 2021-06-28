937bc07e32b7f6d15d8e59ae7f51a0c484d7db8c
dom-codex <dominicwest7@gmail.com>
2021-06-28 19:53:33 +0100

    update code

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
